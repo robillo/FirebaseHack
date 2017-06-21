d0e14d7a5632dd6750d9d075566eec25a5542f46
Rishabh Shukla <rishabhshukla25@hotmail.com>
2017-06-21 12:42:55 +0530

    Phone Auth Done

--- app/src/main/java/com/appbusters/robinkamboj/firebasehack/LoginActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/firebasehack/LoginActivity.java
@@ -1,13 +1,15 @@
 package com.appbusters.robinkamboj.firebasehack;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.PhoneAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
