92963ac420c756af8ceb302ccc3a12c5e3df73b9
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-06-21 14:58:58 +0530

    uploading gov docs

--- app/src/main/java/com/appbusters/robinkamboj/firebasehack/views/activities/ProfileActivity.java
+++ app/src/main/java/com/appbusters/robinkamboj/firebasehack/views/activities/ProfileActivity.java
@@ -1,9 +1,13 @@
 package com.appbusters.robinkamboj.firebasehack.views.activities;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,6 +21,7 @@ import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.appbusters.robinkamboj.firebasehack.R;
 import com.appbusters.robinkamboj.firebasehack.Utils.AppPreferencesHelper;
@@ -26,19 +31,24 @@ import com.facebook.AccessToken;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.HttpMethod;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.json.JSONObject;
 
 import java.util.Calendar;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
@@ -53,11 +63,14 @@ public class ProfileActivity extends AppCompatActivity {
     private StorageReference mStorageReferenceDP,mStorageReferenceCover, mStorageReferenceGovID, mStorageReferenceSpecID;
     private FirebaseAnalytics mFirebaseAnalytics;
     private FirebaseAuth firebaseAuth;
-    private static final int RESULT_LOAD_GOV_ID = 8010, RESULT_LOAD_SPEC_ID = 8011;
+    private static final int RESULT_LOAD_GOV_ID = 8010;
     Uri govPath = null, specPath = null;
     private int code = 0;
     private String pushGender = null, pushID = null, pushName = null, pushUserType =null, pushSpec = null;
-
+    
+    @BindView(R.id.govID)
+    ImageView govID;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -129,15 +142,15 @@ public class ProfileActivity extends AppCompatActivity {
 
         FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
         mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        mUsersDatabase = FirebaseDatabase.getInstance().getReference("users");
+        mUsersDatabase = FirebaseDatabase.getInstance().getReference("students");
         DatabaseReference mTempDatabase = FirebaseDatabase.getInstance().getReference();
-        mTempDatabase.child("gurus").child(mFirebaseUser.getUid()).push();
-        mGurusDatabase = FirebaseDatabase.getInstance().getReference("gurus").child("pending");
+        mTempDatabase.child("teachers").child(mFirebaseUser.getUid()).push();
+        mGurusDatabase = FirebaseDatabase.getInstance().getReference("teachers").child("pending");
 
         Log.e("UID", mFirebaseUser.getUid());
 
-        mStorageReferenceGovID = FirebaseStorage.getInstance().getReference("gurus").child("pending").child("govid");
-        mStorageReferenceSpecID = FirebaseStorage.getInstance().getReference("gurus").child("pending").child("specid");
+        mStorageReferenceGovID = FirebaseStorage.getInstance().getReference("teachers").child("pending").child("govid");
+        mStorageReferenceSpecID = FirebaseStorage.getInstance().getReference("teachers").child("pending").child("specid");
 
         if(code==1){        //FROM HOME
             setNotClickable();
@@ -153,9 +166,6 @@ public class ProfileActivity extends AppCompatActivity {
                     ((RadioButton) findViewById(R.id.standard)).setChecked(true);
                     ((RadioButton) findViewById(R.id.guru)).setChecked(false);
                 }
-            }
-            else {
-
             }
         }
 
@@ -202,6 +212,84 @@ public class ProfileActivity extends AppCompatActivity {
         });
     }
 
+
+    @OnClick(R.id.govID)
+    public void setGovID(){
+        if((ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) || (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)){
+            if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 5);
+            }
+
+            if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 6);
+            }
+        }
+        else {
+            Intent i = new Intent(
+                    Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, RESULT_LOAD_GOV_ID);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_LOAD_GOV_ID && resultCode == RESULT_OK && null != data){
+            govPath = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(govPath,
+                    filePathColumn, null, null, null);
+            if (cursor != null) {
+                cursor.moveToFirst();
+            }
+
+            int columnIndex = cursor != null ? cursor.getColumnIndex(filePathColumn[0]) : 0;
+            String picturePath = cursor != null ? cursor.getString(columnIndex) : null;
+            if (cursor != null) {
+                cursor.close();
+            }
+
+            Log.e("PICTURE PATH GOV", "BEFORE");
+            if (picturePath != null) {
+                Log.e("PICTURE PATH GOV", "file://" + picturePath);
+                Glide.with(this)
+                        .load(picturePath)
+                        .centerCrop()
+                        .into(govID);
+            }
+            Log.e("PICTURE PATH GOV", "AFTER");
+
+
+            if(govPath!=null){
+//                Toast.makeText(getApplicationContext(), R.string.shortly, Toast.LENGTH_SHORT).show();
+                StorageReference riversRef = mStorageReferenceGovID.child(mFirebaseUser.getUid());
+                riversRef.putFile(govPath)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                Toast.makeText(ProfileActivity.this, "Image Uploaded..Changes will be reflected.", Toast.LENGTH_SHORT).show();
+//                                fetchData();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                Toast.makeText(ProfileActivity.this, "Upload Failed.", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            }
+                        });
+                mGurusDatabase.child(mFirebaseUser.getUid()).child("govID").setValue(govPath.toString());
+            }
+        }
+    }
+    
     public void setNotClickable(){
         hidenshowCardMain.setClickable(false);
         hidenshowCardUnwanted.setClickable(false);
